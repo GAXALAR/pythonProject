caed7ad77b29a19d20d9a2173c52a204c7885543
GAXALAR <ay.kosyrev@gmail.com>
2023-03-18 13:00:47 +0300

    корректировка ДЗ

--- task_1.py
+++ task_1.py
@@ -1,6 +1,6 @@
-FirstName = input('Введите имя: ')
-SecondName = input('Введите фамилию: ')
-UserPassword = input('Задайте пароль: ')
-Age = input('Введите ваш возраст: ')
+first_name = input('Введите имя: ')
+second_name = input('Введите фамилию: ')
+user_password = input('Задайте пароль: ')
+age = input('Введите ваш возраст: ')
 
-print(f"Данные аккаунта: имя - {FirstName}, фамилия - {SecondName}, пароль - {UserPassword}, возраст - {Age} лет")
+print(f"Данные аккаунта: имя - {first_name}, фамилия - {second_name}, пароль - {user_password}, возраст - {age} лет")
